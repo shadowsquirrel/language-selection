be231acb33dfb24fb990e9c31372f865135565ce
shadowsquirrel <cnelebi@gmail.com>
2021-12-13 00:09:53 +0100

    -made things a bit more smaller for easy finger space on the right half

--- js/language.js
+++ js/language.js
@@ -2,6 +2,63 @@ window.onload = function() {
 
     // var node = parent.node;
 
+    var revealSelection = function() {
+
+        var delay = 150;
+
+        setTimeout(()=>{
+            $('.0-china').css({'transition':'0.8s', 'opacity':'1'});
+        }, delay)
+
+        setTimeout(()=>{
+            $('.0-germany').css({'transition':'0.8s', 'opacity':'1'});
+        }, 2 * delay)
+
+        setTimeout(()=>{
+            $('.0-english').css({'transition':'0.8s', 'opacity':'1'});
+        }, 3 * delay)
+
+        setTimeout(()=>{
+            $('.0-spain').css({'transition':'0.8s', 'opacity':'1'});
+        }, 4 * delay)
+
+        setTimeout(()=>{
+            $('.0-france').css({'transition':'0.8s', 'opacity':'1'});
+        }, 5 * delay)
+
+        setTimeout(()=>{
+            $('.0-italy').css({'transition':'0.8s', 'opacity':'1'});
+        }, 6 * delay)
+
+        setTimeout(()=>{
+            $('.0-korea').css({'transition':'0.8s', 'opacity':'1'});
+        }, 7 * delay)
+
+        setTimeout(()=>{
+            $('.0-portugal').css({'transition':'0.8s', 'opacity':'1'});
+        }, 8 * delay)
+
+        setTimeout(()=>{
+            $('.0-russia').css({'transition':'0.8s', 'opacity':'1'});
+        }, 9 * delay)
+
+        setTimeout(()=>{
+            $('.0-turkey').css({'transition':'0.8s', 'opacity':'1'});
+        }, 10 * delay)
+
+        setTimeout(()=>{
+            $('.0-pakistan').css({'transition':'0.8s', 'opacity':'1'});
+        }, 11 * delay)
+
+        setTimeout(()=>{
+            $('.0-vietnam').css({'transition':'0.8s', 'opacity':'1'});
+        }, 12 * delay)
+
+    }
+
+    revealSelection();
+
+
     // CHINESE
     $('#china').click(function() {
 
