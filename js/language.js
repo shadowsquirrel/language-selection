e87c8f5f5e2fa2cc948ec5dad4f2c1bc1a8532fd
shadowsquirrel <cnelebi@gmail.com>
2021-12-12 23:44:43 +0100

    -all click functions checked and ready to be used

--- js/language.js
+++ js/language.js
@@ -2,33 +2,100 @@ window.onload = function() {
 
     // var node = parent.node;
 
+    // CHINESE
+    $('#china').click(function() {
 
+        console.log('chinese');
+
+    })
+
+
+    // GERMAN
+    $('#germany').click(function() {
+
+        console.log('german');
+
+    })
+
+
+    // ENGLISH
+    $('#english').click(function() {
+
+        console.log('english');
+
+    })
+
+
+    // SPANISH
+    $('#spain').click(function() {
+
+        console.log('spanish');
+
+    })
+
+
+    // FRENCH
+    $('#france').click(function() {
+
+        console.log('french');
+
+    })
+
+
+    // ITALIAN
+    $('#italy').click(function() {
+
+        console.log('italian');
+
+    })
+
+
+    // KOREAN
+    $('#korea').click(function() {
+
+        console.log('korean');
+
+    })
+
+
+    // PORTUGUESE
+    $('#portugal').click(function() {
+
+        console.log('portuguese');
+
+    })
+
+
+    // RUSSIAN
+    $('#russia').click(function() {
+
+        console.log('russian');
+
+    })
+
+
+    // TURKISH
     $('#turkey').click(function(event) {
 
         console.log('turkish');
 
     })
 
-    $('#italy').click(function() {
 
-        console.log('italy');
+    // URDU
+    $('#pakistan').click(function(event) {
 
-    })
-
-    $('#germany').click(function() {
-
-        console.log('germany');
-
-    })
-
-    $('#spain').click(function() {
-
-        console.log('spain');
+        console.log('urdu');
 
     })
 
 
+    // VIETNAM
+    $('#vietnam').click(function() {
 
-//
+        console.log('vietnamese');
+
+    })
+
 
 }
