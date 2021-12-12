c9634365ae5760416de88a0f9c4f82c8d68e1ade
shadowsquirrel <cnelebi@gmail.com>
2021-12-12 18:02:33 +0100

    asdas

---
+++ js/language.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+
+    // var node = parent.node;
+
+
+    $('#turkey').click(function() {
+
+        console.log('turkish');
+
+    })
+
+    $('#italy').click(function() {
+
+        console.log('italy');
+
+    })
+
+    $('#germany').click(function() {
+
+        console.log('germany');
+
+    })
+
+    $('#spain').click(function() {
+
+        console.log('spain');
+
+    })
+
+
+
+
+}
